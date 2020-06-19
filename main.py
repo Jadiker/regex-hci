238b603e170d50dd12648ad0a9ed9f86e792d1f2
Jadiker <happylondon4@gmail.com>
2020-06-19 15:53:06 -0500

    parser working

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ parser = Lark('''
        | repeat_range
 
     ?c : number_class
+       | non_zero_number_class
        | letter_class
        | lowercase_class
        | uppercase_class
@@ -25,12 +26,13 @@ parser = Lark('''
 
     k : INT
 
-    number_class         : "<num>"
-    letter_class         : "<let>"
-    lowercase_class      : "<low>"
-    uppercase_class      : "<cap>"
-    any_class            : "<any>"
-    alphanumerical_class : "<alphanum>"
+    number_class          : "<num>"
+    non_zero_number_class : "<num1-9>"
+    letter_class          : "<let>"
+    lowercase_class       : "<low>"
+    uppercase_class       : "<cap>"
+    any_class             : "<any>"
+    alphanumerical_class  : "<alphanum>"
 
     number: "<" INT ">"
     letter: "<" LETTER ">"
@@ -51,3 +53,53 @@ parser = Lark('''
 
 parse_tree = parser.parse("concat(repeatatleast(<let>,1),<C>)")
 print(parse_tree.pretty())
+
+def construct_regex(tree):
+    operation = tree.data
+    if operation == "start":
+        return construct_regex(tree.children[0])
+    if operation == "concat":
+        child1, child2 = tree.children
+        return construct_regex(child1) + construct_regex(child2)
+    if operation == "repeat_at_least":
+        r_tree, k_tree = tree.children
+        repeatable_regex = construct_regex(r_tree)
+        # k's child will be a token that we can convert to a string.
+        amount = int(str(k_tree.children[0]))
+        # repeat the regex the given amount of times and then make any more repetitions optional
+        return (repeatable_regex * amount) + f"(?:{repeatable_regex})*"
+
+    if operation == "letter_class":
+        return "[a-zA-Z]"
+
+    if operation == "letter":
+        # turn the letter Token into a string
+        return str(tree.children[0])
+
+    print(f"Didn't know what to do for:\n{tree}\n(Putting in a '<something>')")
+    return "<something>"
+
+regex = construct_regex(parse_tree)
+print(regex)
+
+# print(parse_tree.data)
+# print(type(parse_tree.data))
+# print(parse_tree.children)
+# print(dir(parse_tree))
+
+# print(type(parse_tree))
+#
+# print(list(parse_tree.iter_subtrees_topdown()))
+#
+# print("LOOP")
+# for thing in parse_tree.iter_subtrees_topdown():
+#     # print(thing)
+#     print(thing.data)
+#     # print("children:")
+#     # print(thing.children)
+#     # print("children done")
+#     # if thing.data == "k":
+#     #     print(thing)
+#     #     print(thing.children[0])
+#     #     print(type(thing.children[0]))
+#     # # print(type(thing))
